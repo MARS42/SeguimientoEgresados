7862eb34a9e0d14ddb01b19f91674551880d035f
Roberto Martinez <msjred42@gmail.com>
2022-02-22 13:55:42 -0600

    Creación del proyecto ASP.Net Core

---
+++ SeguimientoEgresados/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SeguimientoEgresados.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
