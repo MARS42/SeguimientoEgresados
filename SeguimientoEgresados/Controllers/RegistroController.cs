73c5ea259792b2726941221c59f79ea388754495
Robert Mtz <msjred42@gmail.com>
2022-03-27 17:35:53 -0600

    Creacion de controlador para registro de usuario: Registrar

---
+++ SeguimientoEgresados/Controllers/RegistroController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeguimientoEgresados.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Empleador()
+        {
+            return View();
+        }
+    }
+}
