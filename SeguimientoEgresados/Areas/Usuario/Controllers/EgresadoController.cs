d4b2ed162ab9b5074bdd5fbcbf50b40843135b35
Robert Mtz <msjred42@gmail.com>
2022-04-07 02:36:26 -0500

    Actualización de datos básicos egresado

--- SeguimientoEgresados/Areas/Usuario/Controllers/EgresadoController.cs
+++ SeguimientoEgresados/Areas/Usuario/Controllers/EgresadoController.cs
@@ -1,13 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeguimientoEgresados.Models;
+using SeguimientoEgresados.Utils;
 
 namespace SeguimientoEgresados.Areas.Usuario.Controllers
 {
     [Area("Usuario")]
     public class EgresadoController : Controller
     {
-        public IActionResult Index()
+        private readonly SeguimientoEgresadosContext _context;
+
+        public EgresadoController(SeguimientoEgresadosContext context)
         {
-            return View();
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            Models.Usuario? user = HttpContext.Session.Get<Models.Usuario>("User");
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id.Equals(user!.Id));
+            //var cuestionario = await _context.Cuestionarios.FirstOrDefaultAsync(c => c.IdUsuario.Equals(usuario!.Id));
+            //Console.WriteLine("cues: " + cuestionario);
+            //ViewData["Cuestionario"] = cuestionario;
+            return View(usuario);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Index(Models.Usuario model)
+        {
+            Models.Usuario? user = HttpContext.Session.Get<Models.Usuario>("User");
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id.Equals(user!.Id));
+
+            usuario.Nombre = model.Nombre;
+            usuario.ApellidoPaterno = model.ApellidoPaterno;
+            usuario.ApellidoMaterno = model.ApellidoMaterno;
+            //usuario.Email = model.Email;
+            
+            _context.Entry(usuario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> MisDatos()
