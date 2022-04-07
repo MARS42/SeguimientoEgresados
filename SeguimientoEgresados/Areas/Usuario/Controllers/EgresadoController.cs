43a79b5e803e1ba4be0e56aa4cc3a6cd98c10edb
Robert Mtz <msjred42@gmail.com>
2022-04-07 03:02:51 -0500

    Actualizaicón de datos de egresado terminado

--- SeguimientoEgresados/Areas/Usuario/Controllers/EgresadoController.cs
+++ SeguimientoEgresados/Areas/Usuario/Controllers/EgresadoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SeguimientoEgresados.Models;
 using SeguimientoEgresados.Utils;
@@ -43,7 +44,48 @@ namespace SeguimientoEgresados.Areas.Usuario.Controllers
 
         public async Task<IActionResult> MisDatos()
         {
-            return View();
+            Models.Usuario? user = HttpContext.Session.Get<Models.Usuario>("User");
+            var egresado = await _context.Egresados.FirstOrDefaultAsync(e => e.IdUsuario.Equals(user!.Id));
+            ViewData["Generos"] = new SelectList(_context.Generos, "Id", "Nombre");
+            ViewData["EstadosCiviles"] = new SelectList(_context.EstadosCiviles, "Id", "Nombre");
+            ViewData["Carreras"] = new SelectList(_context.Carreras, "Id", "Nombre");
+            
+            Console.WriteLine($"User id: {user.Id}, empresa id: {egresado.NControl}");
+            
+            return View(egresado);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> MisDatos(Egresado model)
+        {
+            Models.Usuario? user = HttpContext.Session.Get<Models.Usuario>("User");
+            var egresado = await _context.Egresados.FirstOrDefaultAsync(e => e.IdUsuario.Equals(user!.Id));
+
+            egresado.Colonia = model.Colonia;
+            egresado.Cp = model.Cp;
+            egresado.Domicilio = model.Domicilio;
+            egresado.Estado = model.Estado;
+            egresado.Municipio = model.Municipio;
+            egresado.Pais = model.Pais;
+            
+            egresado.FechaEgreso = model.FechaEgreso;
+            egresado.FechaNacimiento = model.FechaNacimiento;
+            
+            egresado.PaisNacimiento = model.PaisNacimiento;
+            egresado.MunicipioNacimiento = model.MunicipioNacimiento;
+            egresado.EstadoNacimiento = model.EstadoNacimiento;
+
+            egresado.IdGenero = model.IdGenero;
+            egresado.IdCarrera = model.IdCarrera;
+            egresado.IdEstadoCivil = model.IdEstadoCivil;
+
+            egresado.NControl = model.NControl;
+            egresado.Telefono = model.Telefono;
+            
+            _context.Entry(egresado).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            //return Ok(model);
+            return RedirectToAction("MisDatos");
         }
 
         public IActionResult Cuestionario()
