786e4f4bca00035d5be2a1a3f926c2336a021d80
MARS42 <msjred42@gmail.com>
2022-04-10 04:11:31 -0500

    Cambios conexcion a formulario

--- SeguimientoEgresados/Services/GoogleSheetsService.cs
+++ SeguimientoEgresados/Services/GoogleSheetsService.cs
@@ -3,6 +3,7 @@ using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Util.Store;
+using Newtonsoft.Json;
 using System.Configuration;
 
 namespace SeguimientoEgresados.Services;
@@ -45,27 +46,52 @@ public class GoogleSheetsService : IGoogleSheetsService
     public async Task<string> VerificarCuestionario(string email)
     {
         String spreadsheetId = "1UmZ1fropK_rOYmZ-5eDtTGoJIrP4bb4KtgNLgcgQDxA";
+
+        List<Google.Apis.Sheets.v4.Data.Request> requests = new List<Google.Apis.Sheets.v4.Data.Request>();
         
-        String range = "Respuestas de formulario 1!B2:B20";
-        
-        SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
-        
-        ValueRange response = await request.ExecuteAsync();
-        
-        IList<IList<Object>> values = response.Values;
-        
-        if (values != null && values.Count > 0)
-        {
-            Console.WriteLine("Name, Major");
-            if (values.SelectMany(row => row.Cast<string>()).Any(cell => cell.Equals(email)))
-            {
-                return "Ok";
-            }
-        }
-        else
-        {
-            return "Data are null o equals to 0";
-        }
+        requests.Add(new Google.Apis.Sheets.v4.Data.Request() {
+
+        });
+
+
+        // TODO: Assign values to desired properties of `requestBody`:
+        Google.Apis.Sheets.v4.Data.BatchUpdateSpreadsheetRequest requestBody = new Google.Apis.Sheets.v4.Data.BatchUpdateSpreadsheetRequest();
+        requestBody.Requests = requests;
+
+        SpreadsheetsResource.BatchUpdateRequest request = service.Spreadsheets.BatchUpdate(requestBody, spreadsheetId);
+
+        // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+        Google.Apis.Sheets.v4.Data.BatchUpdateSpreadsheetResponse response = await request.ExecuteAsync();
+
+        Console.WriteLine(JsonConvert.SerializeObject(response));
+        // Data.BatchUpdateSpreadsheetResponse response = await request.ExecuteAsync();
+
+        //String range = "Respuestas de formulario 1!A2:B20";
+
+        //SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+        //ValueRange response = await request.ExecuteAsync();
+
+        //IList<IList<Object>> values = response.Values;
+
+        //if (values != null && values.Count > 0)
+        //{
+        //    //Console.WriteLine("Name, Major");
+        //    //if (values.SelectMany(row => row.Cast<string>()).Any(cell => cell.Equals(email)))
+        //    //{
+        //    //    return "Ok";
+        //    //}
+        //    foreach(var fila in values)
+        //    {
+        //        string fecha = (string)fila[0];
+        //        //string emailf = (string)fila[1];
+        //        Console.WriteLine($"{fecha} - {email}");
+        //    }
+        //}
+        //else
+        //{
+        //    return "Data are null o equals to 0";
+        //}
         return "Error";
     }
 }
