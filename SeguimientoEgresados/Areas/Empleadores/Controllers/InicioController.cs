d238a7a426f6148a0d0865e70b8cc6b38422d954
MARS42 <msjred42@gmail.com>
2022-04-18 23:46:22 -0500

    Visualización de empleadores para visitantes

--- SeguimientoEgresados/Areas/Empleadores/Controllers/InicioController.cs
+++ SeguimientoEgresados/Areas/Empleadores/Controllers/InicioController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeguimientoEgresados.Models;
+using SeguimientoEgresados.Models.ViewModels;
 using SeguimientoEgresados.Services;
 
 namespace SeguimientoEgresados.Areas.Empleadores.Controllers
@@ -10,18 +13,41 @@ namespace SeguimientoEgresados.Areas.Empleadores.Controllers
     [Area("Empleadores")]
     public class InicioController : Controller
     {
+        private readonly SeguimientoEgresadosContext _db;
         private readonly INotificacionesService _notificaciones;
         
-        public InicioController(INotificacionesService notificaciones)
+        public InicioController(SeguimientoEgresadosContext db, INotificacionesService notificaciones)
         {
+            _db = db;
             _notificaciones = notificaciones;
         }
         
-        [HttpGet, Route("~/Empleadores/")]
+        // [HttpGet, Route("~/Empleadores/")]
         public async Task<IActionResult> Index()
         {
+            var query =
+                from empresa in _db.Empresas
+                join usuario in _db.Usuarios on empresa.IdUsuario equals usuario.Id into empresasUsuario
+                from empresaUsuario in empresasUsuario.DefaultIfEmpty()
+                select new VisitaEmpresaViewModel(empresaUsuario.Id, empresa.Id)
+                {
+                    Nombre = empresa.Nombre,
+                    Descripcion = "",
+                    Correo = empresa.CorreoEmpresa,
+                    Telefono = empresa.Telefono,
+                    Website = empresa.Website,
+                    Rfc = empresa.Rfc,
+                    FechaRegistro = empresaUsuario.Fecha,
+                    NombreRep = empresaUsuario.Nombre,
+                    Ape1Rep = empresaUsuario.ApellidoPaterno,
+                    Ape2Rep = empresaUsuario.ApellidoMaterno,
+                    LogoImagen = empresaUsuario.UrlImg
+                };
+
+            query = query.OrderBy(e => e.Nombre);
+            
             await _notificaciones.VerificarCuestionario(User, HttpContext, ViewData, false);
-            return View();
+            return View(await query.AsNoTracking().ToListAsync());
         }
     }
 }
---
+++ SeguimientoEgresados/Models/ViewModels/VisitaEmpresaViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace SeguimientoEgresados.Models.ViewModels;
+
+public class VisitaEmpresaViewModel
+{
+    public readonly int idUsuario, idEmpresa;
+    
+    public string Nombre { get; set; }
+    
+    public string Descripcion { get; set; }
+    
+    public string Rfc { get; set; }
+    public string Correo { get; set; }
+    
+    public string Telefono { get; set; }
+    
+    public string Website { get; set; }
+    
+    public string LogoImagen { get; set; }
+    
+    public string NombreRep { get; set; }
+    public string Ape1Rep { get; set; }
+    public string Ape2Rep { get; set; }
+    
+    public string Representante
+    {
+        get => $"{NombreRep} {Ape1Rep} {Ape2Rep}";
+    }
+    
+    public DateTime FechaRegistro { get; set; }
+
+    public VisitaEmpresaViewModel(int idUsuario, int idEmpresa)
+    {
+        this.idEmpresa = idEmpresa;
+        this.idUsuario = idUsuario;
+    }
+}
